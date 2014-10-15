8ce762079445aa6a2eb6faf5db5d9a7a3aebdacd
Scott Lillibridge <slillibridge@whitepages.com>
2014-10-15 08:48:51 -0700

    Set date to UTC

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 listen_host = '0.0.0.0'  # defaults to "all interfaces"
 listen_port = 8081
 opsreport_start = '01/01/2013 12:00:00 AM'
-timezone = "America/New_York"
+timezone = "Etc/UTC"
 es_url = 'http://localhost:9200'
 es_index = 'anthracite'
 
